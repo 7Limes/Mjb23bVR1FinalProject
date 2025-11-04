37df54be34de064919cc4e1ad7a39ae82f1cf6ed
Miles Burkart <milesb2288@gmail.com>
2025-11-03 22:25:29 -0500

    Add flick detection

--- Assets/Scripts/Wand.cs
+++ Assets/Scripts/Wand.cs
@@ -1,14 +1,30 @@
-using System;
 using UnityEngine;
-using UnityEngine.UIElements;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class Wand : MonoBehaviour {
+    [SerializeField] private float holdDistance = 0.05f;  // The maximum distance at which the wand is considered "held"
+    
     [SerializeField] private float rotateSpeed = 0.25f;
     [SerializeField] private float oscillateSpeed = 1.0f;
     [SerializeField] private float oscillateAmplitude = 0.05f;
 
+    [SerializeField] private float flickThreshold = 250f;
+    [SerializeField] private float castCooldown = 1.0f;
+
     private GameObject wandModel;
-    private bool isGrabbed = false;
+    private IXRSelectInteractor currentInteractor = null;
+
+    private bool isGrabbed = false;  // Whether the wand has been grabbed ("selected")
+    private bool isHeld = false;     // Whether the wand is actually in the player's hand
+
+    const int ANGULAR_VELOCITY_BUFFER_SIZE = 10;
+    private Queue<Vector3> angularVelocityBuffer = new Queue<Vector3>(ANGULAR_VELOCITY_BUFFER_SIZE);
+    private Quaternion previousRotation = Quaternion.Euler(0, 0, 0);
+    private Vector3 smoothedAngularVelocity = Vector3.zero;
+
+    private float castTimer = 0.0f;
 
     public void SetWandModel(GameObject wandModel) {
         this.wandModel = wandModel;
@@ -16,13 +32,46 @@ public class Wand : MonoBehaviour {
     
     public void OnGrab() {
         isGrabbed = true;
+        currentInteractor = GetComponent<XRGrabInteractable>().firstInteractorSelecting;
     }
 
     public void OnRelease() {
         isGrabbed = false;
+        isHeld = false;
+        currentInteractor = null;
         wandModel.transform.localPosition = Vector3.zero;
         wandModel.transform.localRotation = Quaternion.Euler(0, 0, 0);
         transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    void Start() {
+        // Fill angular velocity buffer with zeroed vectors
+        for (int i = 0; i < ANGULAR_VELOCITY_BUFFER_SIZE; i++) {
+            angularVelocityBuffer.Enqueue(Vector3.zero);
+        }
+    }
+    
+    void UpdateAngularVelocity() {
+        Quaternion currentRotation = transform.rotation;
+        Quaternion deltaRotation = currentRotation * Quaternion.Inverse(previousRotation);
+        float angle;
+        Vector3 axis;
+        deltaRotation.ToAngleAxis(out angle, out axis);
+
+        if (angle > 180) {
+            angle -= 360;
+        }
+
+        Vector3 angularVelocity = axis * (angle / Time.fixedDeltaTime);
+        angularVelocityBuffer.Dequeue();
+        angularVelocityBuffer.Enqueue(angularVelocity);
+        previousRotation = currentRotation;
+
+        smoothedAngularVelocity = Vector3.zero;
+        foreach (Vector3 v in angularVelocityBuffer) {
+            smoothedAngularVelocity += v;
+        }
+        smoothedAngularVelocity /= ANGULAR_VELOCITY_BUFFER_SIZE;
     }
 
     void Update() {
@@ -33,6 +82,29 @@ public class Wand : MonoBehaviour {
             float oscillateY = oscillateAmplitude * Mathf.Sin(Time.fixedTime * oscillateSpeed);
             Vector3 oscillatePosition = new Vector3(0, oscillateY, 0);
             wandModel.transform.localPosition = oscillatePosition;
+        }
+    }
+    
+
+    void FixedUpdate() {
+        if (castTimer > 0) {
+            castTimer = Mathf.MoveTowards(castTimer, 0.0f, Time.fixedDeltaTime);
+        }
+        
+        if (isGrabbed) {
+            // Update held status
+            if (currentInteractor != null) {
+                float distance = Vector3.Distance(currentInteractor.transform.position, transform.position);
+                isHeld = distance < holdDistance;
+            }
+
+            UpdateAngularVelocity();
+
+            // Check for flick
+            if (isHeld && smoothedAngularVelocity.magnitude > flickThreshold && castTimer == 0) {
+                Debug.Log("flicked!");
+                castTimer = castCooldown;
+            }
         }
     }
 }
