33be55062ebb1206da2324a3d5a3b25b7029ee2f
Rubini 07 <user@FIH-COM23a107.fsu.edu>
2025-11-19 15:53:33 -0500

    Implement structure menu

--- Assets/Scripts/Structures/StructureMenu.cs
+++ Assets/Scripts/Structures/StructureMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 
@@ -6,10 +7,23 @@ public class StructureMenu : MonoBehaviour {
     [SerializeField] private GameObject structureListContent;
     [SerializeField] private GameObject structureButtonPrefab;
 
+    [SerializeField] private Transform structureSpawnPoint;
+
     [SerializeField] private Vector2 structureListOffset = Vector2.zero;
     [SerializeField] private float structureListSpacing = 0.05f;
     
     [SerializeField] private List<StructureData> structures = new List<StructureData>();
+
+    public void ClearStructure() {
+        foreach (Transform child in structureSpawnPoint.transform) {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void InstantiateStructure(GameObject prefab) {
+        ClearStructure();
+        Instantiate(prefab, structureSpawnPoint);
+    }
 
     void Start() {
         Vector3 translationVector = new Vector3(structureListOffset.x, structureListOffset.y, 0);
@@ -20,6 +34,12 @@ public class StructureMenu : MonoBehaviour {
 
             var buttonText = structureButton.GetComponentInChildren<TextMeshProUGUI>();
             buttonText.SetText(structure.structureName);
+               
+            // Bind events to buttons
+            var buttonScript = structureButton.GetComponent<Button>();
+            buttonScript.onClick.AddListener(() => {
+                InstantiateStructure(structure.prefab);
+            });
         }
     }
 }
