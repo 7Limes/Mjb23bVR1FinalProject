b136eafe5ac589a1bd5f0c179f1c132772e2c21b
Miles Burkart <milesb2288@gmail.com>
2025-12-01 00:11:30 -0500

    Fix death box deleting player
    ~ Adjust projectile spell cube material
    ~ Reduce capsule trigger break force

--- Assets/Scripts/Spells/Teleport/TeleportPlayer.cs
+++ Assets/Scripts/Spells/Teleport/TeleportPlayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class TeleportPlayer : Projectile {
     void Start() {
         transform.rotation = Quaternion.Euler(0, 0, 0);
-        GameObject caster = GameObject.FindGameObjectWithTag("Player");
+        GameObject caster = GameObject.FindGameObjectWithTag("XRRig");
         if (caster != null) {
             caster.transform.position = transform.position;
         }
